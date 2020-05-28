16f6a71d2cfc37326ba1f992eb0473b6d775059e
Nick Zhu <xinrzhu@microsoft.com>
2020-05-28 17:49:53 +0800

    progress

--- dotnet/Track2.cs
+++ dotnet/Track2.cs
@@ -3,7 +3,9 @@ using System;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Collections;
+using System.Collections.Generic;
 
+using Azure.Core.Pipeline;
 using Azure.Identity;
 using Azure.Management.Compute;
 using Azure.Management.Network;
@@ -15,15 +17,34 @@ namespace Track2UXCodeSample {
     {
         public static async Task CreateResourceAsync(
             string subscriptionId,
-            string resourceGroup,
+            string resourceGroupName,
             string location)
         {
             Console.WriteLine("test");
             var credentials = new DefaultAzureCredential(true);
+            
+            //create
             var resourceGroupClient = new ResourcesManagementClient(subscriptionId, credentials).GetResourceGroupsClient();
-            var createdResourceGroup = await resourceGroupClient.CreateOrUpdateAsync("test_rng_name", new ResourceGroup(location));
+            var resourceGroup = new ResourceGroup(location);
+            resourceGroup = await resourceGroupClient.CreateOrUpdateAsync("test_rng_name", resourceGroup);
+            //var createdResourceGroup = res.Value;
 
-            Console.WriteLine(createdResourceGroup.Value.Name);
+            Console.WriteLine(resourceGroup.Name);
+
+            //update
+            var tags = new Dictionary<string, string>();
+            tags.Add("env","prod");
+            tags.Add("scenario","test");
+            resourceGroup.Tags = tags;
+            var updatedResourceGroup = await resourceGroupClient.CreateOrUpdateAsync("test_rng_name", resourceGroup);
+
+
+            //get
+            AsyncPageable<ResourceGroup> response = await resourceGroupClient.ListAsync();
+            await foreach (ResourceGroupPatchable resourceGroup in response) {
+                Console.WriteLine(resourceGroup.Name);
+            }
+
 
 
         }
